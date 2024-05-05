b44d83e94e10abd9b6d263c2ea5c252c891b14d2
Rivaldito <araquesebastian@gmail.com>
2024-05-05 12:13:23 -0400

    all done

--- Python/settings.py
+++ Python/settings.py
@@ -1,12 +1,8 @@
 
-# publicEndPoint = "YourURLToConnectTOTheDataBase"
-# port           =  000
-# password       = "YourPassword"
-# username       = "default"
-
-
-publicEndPoint = "redis-12806.c239.us-east-1-2.ec2.cloud.redislabs.com"
-port           =  12806
-password       = "TbcHu6lqED13k7S4szNh9ykPY9m16gOM"
+publicEndPoint = "YourURLToConnectTOTheDataBase"
+port           =  000
+password       = "YourPassword"
 username       = "default"
+
+
 
